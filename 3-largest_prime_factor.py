c7c32a7832ed788273899853885956a10ff90006
paul-talma <109061871+paul-talma@users.noreply.github.com>
2024-07-03 23:18:22 -0400

    optimizations

--- 3-largest_prime_factor.py
+++ 3-largest_prime_factor.py
@@ -3,17 +3,67 @@
 import math
 
 
-def all_primes_below(n):
-    primes = []
-    for i in range(2, n):
-        if all([i % k != 0 for k in primes]):
-            primes.append(i)
-    return primes
+def factors(n):
+    factors = []
+    current_factor = 2
+    if n % current_factor == 0:
+        while n % current_factor == 0:
+            factors.append(current_factor)
+            n //= current_factor
+
+    current_factor = 3
+    while n > 1:
+        if n % current_factor == 0:
+            while n % current_factor == 0:
+                factors.append(current_factor)
+                n //= current_factor
+        current_factor += 2
+    return factors
 
 
-def prime_factors(n):
-    return [p for p in all_primes_below(int(math.sqrt(n))) if n % p == 0]
+def unique_factors(n):
+    factors = []
+    current_factor = 2
+    current_factor = 2
+    if n % current_factor == 0:
+        while n % current_factor == 0:
+            factors.append(current_factor)
+            n //= current_factor
+
+    current_factor = 3
+    while n > 1:
+        if n % current_factor == 0:
+            factors.append(current_factor)
+            while n % current_factor == 0:
+                n //= current_factor
+        current_factor += 1
+    return factors
+
+
+def largest_factor(n):
+    largest = 1
+    current_factor = 2
+    if n % current_factor == 0:
+        while n % current_factor == 0:
+            n //= current_factor
+        largest = 2
+
+    current_factor = 3
+    max_factor = math.sqrt(n)
+    while n > 1 and current_factor <= max_factor:
+        if n % current_factor == 0:
+            while n % current_factor == 0:
+                n //= current_factor
+            largest = current_factor
+            max_factor = math.sqrt(n)
+        current_factor += 2
+    if n == 1:
+        return largest
+    return n
 
 
 if __name__ == "__main__":
-    print(prime_factors(600_851_475_143))
+    k = 343567
+    print(factors(k))
+    print(unique_factors(k))
+    print(largest_factor(k))
